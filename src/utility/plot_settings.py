72feeab02a27f26a74f840d77577002971eb7967
Marek <mysior.marek@gmail.com>
2024-02-20 13:32:56 +0100

    Mute the colors variable in plot_settings.py

--- src/utility/plot_settings.py
+++ src/utility/plot_settings.py
@@ -3,17 +3,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from cycler import cycler
 
-colors = cycler(color=plt.get_cmap("tab10").colors)  # ["b", "r", "g"]
+# colors = cycler(color=plt.get_cmap("tab10").colors)  # ["b", "r", "g"]
 # colors = cycler(color=["#282782", "r", "g"])
 
-cm = 1 / 2.54  # cm to inch
-
 mpl.style.use("ggplot")
-mpl.rcParams["figure.figsize"] = (16*cm, 9*cm)
+mpl.rcParams["figure.figsize"] = (16 / 2.54, 9 / 2.54)
 mpl.rcParams["axes.facecolor"] = "white"
 mpl.rcParams["axes.grid"] = True
 mpl.rcParams["grid.color"] = "lightgray"
-mpl.rcParams["axes.prop_cycle"] = colors
+# mpl.rcParams["axes.prop_cycle"] = colors
 mpl.rcParams["axes.linewidth"] = 1
 mpl.rcParams["xtick.color"] = "black"
 mpl.rcParams["ytick.color"] = "black"
